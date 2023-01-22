7037f32ee0cbe2d3cca9ecc93b73db11ec371f99
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2023-01-22 10:59:19 -0800

    rm-rf sanity git ignore

---
+++ sanity-project/dist/static/index-6ea4c5fb-2388778e.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-66ec8894-afb884b1.js";import"./desk-eb7b3548-f68967be.js";import"./index-7637b248.js";export{m as DocumentPaneProvider,a as default};
